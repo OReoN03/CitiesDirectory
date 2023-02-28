c39ac50b0c8ff2d1f7a48a7e64bc2df0b0079b4a
OReoN03 <gorkov.artem03@mail.ru>
2023-02-28 18:59:29 +0300

    Сортировать список городов
    Класс CitiesDirectory загружает из файла и хранит список городов. Метод sortCitiesByName() сортирует города по названию, метод sortCitiesByDistrictAndName() сортирует города по федеральному округу и названию.
    
    Полученный список объектов City выводится в консоль.
    
    Класс City был отделён. Добавлены методы equals() и hashcode()

---
+++ src/main/java/org/example/CitiesDirectory.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class CitiesDirectory {
+    private static List<City> cities = new ArrayList<>();
+
+    public static List<City> getCities() {
+        return cities;
+    }
+
+    public static void loadCitiesFromFile(String path) {
+        try {
+            Scanner scanner = new Scanner(Path.of(path));
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] cityFields = line.split(";");
+
+                City city = new City();
+                city.setName(cityFields[1]);
+                city.setRegion(cityFields[2]);
+                city.setDistrict(cityFields[3]);
+                city.setPopulation(Long.parseLong(cityFields[4]));
+                if (cityFields.length > 5) city.setFoundation(cityFields[5]);
+
+                cities.add(city);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<City> sortCitiesByName() {
+        return cities.stream()
+                .sorted(Comparator.comparing(City::getName, String.CASE_INSENSITIVE_ORDER))
+                .toList();
+    }
+
+    public static List<City> sortCitiesByDistrictAndName() {
+        return cities.stream()
+                .sorted(Comparator.comparing(City::getName))
+                .sorted(Comparator.comparing(City::getDistrict))
+                .toList();
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,53 +1,25 @@
 package org.example;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
-    public static class City {
-        private String name;
-        private String region;
-        private String district;
-        private long population;
-        private String foundation;
-
-        public City() {}
-
-        @Override
-        public String toString() {
-            return "City{" +
-                    "name='" + name + '\'' +
-                    ", region='" + region + '\'' +
-                    ", district='" + district + '\'' +
-                    ", population=" + population +
-                    ", foundation='" + foundation + '\'' +
-                    '}';
-        }
-    }
     public static void main(String[] args) {
         String path = "src/main/resources/Задача ВС Java Сбер.csv";
-        List<City> cities = new ArrayList<>();
-        try {
-            Scanner scanner = new Scanner(Path.of(path));
-            while (scanner.hasNext()) {
-                String line = scanner.nextLine();
-                String[] cityFields = line.split(";");
+        CitiesDirectory.loadCitiesFromFile(path);
 
-                City city = new City();
-                city.name = cityFields[1];
-                city.region = cityFields[2];
-                city.district = cityFields[3];
-                city.population = Long.parseLong(cityFields[4]);
-                if (cityFields.length > 5) city.foundation = cityFields[5];
-
-                cities.add(city);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        List<City> cities = CitiesDirectory.getCities();
+        for (City city : cities) {
+            System.out.println(city);
         }
+        System.out.println();
+
+        cities = CitiesDirectory.sortCitiesByName();
+        for (City city : cities) {
+            System.out.println(city);
+        }
+        System.out.println();
+
+        cities = CitiesDirectory.sortCitiesByDistrictAndName();
         for (City city : cities) {
             System.out.println(city);
         }
